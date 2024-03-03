97b888585cc0bbc25a8c1a1ceec0801ada38dfe9
Bhabnashre Dutta Roy <duttaroybhabnashre28@gmail.com>
2024-03-03 23:12:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -15,13 +15,36 @@
  */
  //1st appr:normal bfs, queue size cal...
  //2nd root,right,left bfs  //both has same complexity 0(N)
+ 
+ 
  //3rd DFS+recursion
 class Solution { 
-    int maxDepth=-1;
-    int ans=-1;
+    int maxDepth=0;
+    int ans=0;
     public int findBottomLeftValue(TreeNode root) {
         dfs(root,0);
         return ans;
+        /*time n = number of nodes
+        space m = max number of nodes in each level
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+
+        que.add(root);
+        int leftMost = 0;
+        while (!que.isEmpty()) {
+        TreeNode current = que.poll();
+        leftMost = current.val;
+        if (current.right != null) {
+            que.add(current.right);
+        }
+        if (current.left != null) {
+            que.add(current.left);
+        }
+        }
+        return leftMost;*/
+
+
+
+
     }
     public void dfs(TreeNode root,int depth)
     {
