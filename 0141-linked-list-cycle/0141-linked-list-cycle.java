e44488e28d4c90a548ab1ee2f11637cdfbc3420d
Bhabnashre Dutta Roy <duttaroybhabnashre28@gmail.com>
2024-03-06 21:05:39 +0530

    Time: 0 ms (100%), Space: 44 MB (87.92%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+//1.Marker approach..but the list is need to tweek or copied
+//2.DUPLICATE-HASHING,HASHSET ...again extra space , O(N)
+//3.Floyd's  cycling Algo
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode slow= head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+
+
+
+    }
+}
