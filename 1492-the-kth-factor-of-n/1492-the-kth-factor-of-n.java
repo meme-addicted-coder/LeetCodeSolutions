9eb8b52cc39caa8dad231be35ddb7fc435bcd9fb
Bhabnashre Dutta Roy <duttaroybhabnashre28@gmail.com>
2024-03-10 15:40:55 +0530

    Time: 0 ms (100%), Space: 40.4 MB (46.48%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,6 +1,6 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        int kfact=1;
+       /* int kfact=1;
         int output=-1;
         if(k==1) return kfact;
         for(int i=2;i<=n/2;i++){
@@ -14,6 +14,28 @@ class Solution {
         if(output==-1 && k==kfact+1){
             output=n;
         }
-        return output;
+        return output;*/
+
+        //advanced approach
+        /*Let's assume n is 12. The divisors of n are: {1, 2, 3, 4, 6, 12}. But now look at them from another angle:
+            1 * 12 = 12
+            2 * 6 = 12
+            3 * 4 = 12
+            If i is a divisor of n, then n / i is a divisor of n as well. 
+            Consequently, we can find all the divisors of n only by processing the numbers that are smaller or 
+            equal to the square root of n.*/
+        for(int i = 1; i < Math.sqrt(n); ++i){
+            if(n % i== 0 && --k == 0){
+                return i;
+            }    
+        } 
+	                        
+        for(int i = (int) Math.sqrt(n); i >= 1; --i) {
+            if(n % (n/i) == 0 && --k == 0){
+                return n/i;
+            }
+        }
+              
+        return -1;
     }
 }
