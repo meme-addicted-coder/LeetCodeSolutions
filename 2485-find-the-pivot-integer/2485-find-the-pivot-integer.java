badcc5314a012fb6c546102a75446253c059db41
Bhabnashre Dutta Roy <duttaroybhabnashre28@gmail.com>
2024-03-13 21:01:49 +0530

    Time: 1 ms (77.54%), Space: 40.3 MB (78.99%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,18 @@
+//brute force approach: O(n^2)
+//Optimized: O(n)
+//1. LS=0 RS=n*(n+1)/2 then traverse k till if LS=RS
+//mathematical approach: pivot= (int)root of sum,if pivot*pivot==sum ,pivot exists
+class Solution {
+    public int pivotInteger(int n) {
+        int ls=0;
+        int rs=n*(n+1)/2;
+        for(int k=1;k<=n;k++){
+            ls+=k;
+            rs-=(k-1);
+            if(ls==rs){
+                return k;
+            }
+        }
+        return -1;
+    }
+}
